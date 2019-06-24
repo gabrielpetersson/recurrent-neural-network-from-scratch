7c0f837db74b03eb8c176e317ce9d95dbc64ea45
Gabriel Petersson <46445785+gabrielpetersson@users.noreply.github.com>
2019-06-24 12:03:23 +0200

    Added details

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='RnnGen',
-    version='1.1',
+    version='1.1.2',
     packages=['rnngen', 'rnngen/word2vec', 'rnngen/processing', 'rnngen/recurrentnetwork',
               'rnngen/resources', 'rnngen/misc', 'rnngen/predict'],
 
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='MIT',
     long_description='Recurrent Neural Network Generative Model with Word2Vec',
     install_requires=[
                     'numpy',
