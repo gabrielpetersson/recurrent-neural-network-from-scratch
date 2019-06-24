aeafeae020d3c43861ef213bca7689a2343e423f
Gabriel Petersson <46445785+gabrielpetersson@users.noreply.github.com>
2019-06-24 12:10:51 +0200

    Added 'greedy prediction''

--- predict/generator.py
+++ predict/generator.py
@@ -25,35 +25,41 @@ def generate_words(seed, input_hidden, hidden_hidden, bh, by, hidden_output, dic
     if training_type == 'words':
         if use_word2vec:
             x = emb[seed]
+
         else:
             x = np.zeros((vocab_size,))
             x[seed] = 1
+        x2 = x
 
         predicted_ids = []
         predicted_ids2 = []
         h = np.zeros((1, nodes))
-
+        h2 = h
         # Generates SAMPLES amount of words
         for t in range(samples):
             # Forward propagation
             h = np.tanh(np.dot(x, input_hidden) + np.dot(hidden_hidden, h[0]) + bh)
             y = np.dot(h, hidden_output) + by
-            y[0][dicts['word_to_id']['#UNK#']] = 0
+
+            h2 = np.tanh(np.dot(x2, input_hidden) + np.dot(hidden_hidden, h2[0]) + bh)
+            y2 = np.dot(h2, hidden_output) + by
 
             # p2 shows what is the word given highest probability
             p = np.exp(y) / np.sum(np.exp(y))
-            p2 = np.zeros(vocab_size)
-            p2[np.argmax(p)] = 1
+            p2 = np.argmax(y2)
 
             # Chooses a word based on probability
             predicted_id = np.random.choice(range(vocab_size), p=p.ravel())
-            predicted_id2 = np.random.choice(range(vocab_size), p=p2.ravel())
+            predicted_id2 = p2
 
             if use_word2vec:
                 x = emb[predicted_id]
+                x2 = emb[predicted_id2]
             else:
                 x = np.zeros((1, vocab_size))
                 x[0][predicted_id] = 1
+                x2 = np.zeros((1, vocab_size))
+                x2[0][predicted_id2] = 1
 
             predicted_ids.append(predicted_id)
             predicted_ids2.append(predicted_id2)
@@ -67,7 +73,7 @@ def generate_words(seed, input_hidden, hidden_hidden, bh, by, hidden_output, dic
 
         # Returns generated text if max number of samples has been reached.
         return dicts['id_to_word'][seed] + ' ' + ' '.join([dicts['id_to_word'][x] for x in predicted_ids]) \
-            + '\n           ' + dicts['id_to_word'][seed] + ' ' \
+            + '\n            ' + dicts['id_to_word'][seed] + ' ' \
             + ' '.join([dicts['id_to_word'][x] for x in predicted_ids2[:-1]])
 
     # Same as words but with letters. Everything is the same unless return statement, I am yet to merge these 2
