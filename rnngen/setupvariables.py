4b12153c66cef08e99722c3f06ae176095ef30f2
Gabriel Petersson <46445785+gabrielpetersson@users.noreply.github.com>
2019-06-25 22:56:34 +0200

    Added window size

--- rnngen/setupvariables.py
+++ rnngen/setupvariables.py
@@ -6,19 +6,22 @@
 # Parameters for training generative model.
 # Quality of predicted text may be increased or decreased immensely if tuning them.
 parameters_setup = {
-    # If the number of words in a sentence is less than MIN_WORDS, the sentence will not be used.
-    # Prevents short non-sense sentences.
-    'LEARNING_RATE': 0.07,
+    # Learning rate is how fast your model is going to learn,
+    # lower to decrease a non convergent model and higher to increase training speed
+    'LEARNING_RATE': 0.0005,
 
     # How many words to look at before altering weights. Low batches may prevent over fit
-    'BATCHES': 1,
+    'BATCHES': 8,
 
     # How many words/letter then model will look at while training.
     # Higher = better at remembering, Lower = very precise but cannot remember.
-    'BP_LOOK_BACK': 4,
+    'BP_LOOK_BACK': 6,
 
     # Number of nodes. More nodes = model will remember contexts, but causes slower training.
-    'NODES': 700
+    'NODES': 2500,
+
+    # Every time RNN generator outputs its prediction, learning rate will be multiplied by this.
+    'LEARNING_RATE_DECREASE': 0.9995
 }
 
 
@@ -27,7 +30,7 @@ parameters_setup = {
 preprocess_setup = {
 
     # If the number of words in a sentence is less than MIN_WORDS, the sentence will not be used.
-    # Prevents short non-sense sentences.
+    # Prevents short non-sense sentences. Default is 5. Should only be changed if TRAINING_TYPE is set to 'letters'.
     'MIN_WORDS': 5,
 
     # If a word appears less than WORD_THRESHOLD in processing, the sentence will not be used
@@ -41,6 +44,7 @@ preprocess_setup = {
     # If 'words' generate words, if 'letters' use letters instead.
     # Increase MIN_WORDS if using 'letters'.
     'TRAINING_TYPE': 'words'
+
 }
 
 
@@ -54,23 +58,26 @@ word2vec_setup = {
     'BATCHES': 1,
 
     # Number of epochs to train. One epoch = going through all text once
-    'EPOCHS': 1,
+    'EPOCHS': 10,
 
     # Only for testing. Instantly finishes word2vec training.
     'SHORT_MODE': False,
 
     # Learning step size
-    'LEARNING_RATE': 0.004,
+    'LEARNING_RATE': 0.2,
 
     # Number of iterations before LR decreases and word2vec verbose
-    'ITERATIONS_BEFORE_LR_DECREASE': 50,
+    'ITERATIONS_BEFORE_LR_DECREASE': 50000,
 
     # For every ITERATIONS_BEFORE_LR_DECREASE multiply LEARNING_RATE with this constant
     # The LR_DECREASE should at the end of training make LEARNING_RATE be around 1/1000 its initial value
     # To calculate a optimal decrease:
     # dataset_size(total iters) / ITERATIONS_BEFORE_LR_DECREASE = number_of_decreases
     # LR_DECREASE ^ number_of_decreases = 0.001
-    'LR_DECREASE': 0.8
+    'LR_DECREASE': 0.995,
+
+    # Decides how many words to look around the original word in word2vec. Default: 1
+    'WINDOW_SIZE': 1
 }
 
 # Parameters for testing of word embedding representations.
@@ -80,7 +87,7 @@ test_emb_setup = {
     'USE_TEST_EMBEDDINGS': True,
 
     # Number of randomly chosen tested embeddings.
-    'NUM_TEST_EMBEDDINGS': 3,
+    'NUM_TEST_EMBEDDINGS': 5,
 
     # List of words to check similarities every time.
     # Recommended to have at least one pair of very similar words like (he, she) which are interchangeable
@@ -95,8 +102,8 @@ test_emb_setup = {
 # (Does not affect quality of generator. Only visual.)
 predict_setup = {
     # Number of words/letters to predict each time
-    'SAMPLES': 15,
+    'SAMPLES': 20,
 
     # Seconds between every output.
-    'SECONDS_BETWEEN_PREDICT': 3
+    'SECONDS_BETWEEN_PREDICT': 10
 }
