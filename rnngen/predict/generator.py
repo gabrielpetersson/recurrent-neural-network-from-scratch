1ba4e76f6a51eed6766163aefedc80103ac53d63
Gabriel Petersson <46445785+gabrielpetersson@users.noreply.github.com>
2019-06-25 21:46:10 +0200

    Added 'greedy' generator

--- rnngen/predict/generator.py
+++ rnngen/predict/generator.py
@@ -20,12 +20,10 @@ def generate_words(seed, input_hidden, hidden_hidden, bh, by, hidden_output, dic
     nodes = predict_setup['NODES']
     use_word2vec = predict_setup['USE_WORD2VEC']
     vocab_size = len(dicts['id_to_word'])
-
     # Embeddings and sparse vector has different inputs
-    if training_type == 'words':
+    if training_type == 'words' or training_type == 'letters':
         if use_word2vec:
             x = emb[seed]
-
         else:
             x = np.zeros((vocab_size,))
             x[seed] = 1
@@ -44,6 +42,8 @@ def generate_words(seed, input_hidden, hidden_hidden, bh, by, hidden_output, dic
             h2 = np.tanh(np.dot(x2, input_hidden) + np.dot(hidden_hidden, h2[0]) + bh)
             y2 = np.dot(h2, hidden_output) + by
 
+            y[0][dicts['word_to_id']['#END#']] = 0
+            y2[0][dicts['word_to_id']['#END#']] = 0
             # p2 shows what is the word given highest probability
             p = np.exp(y) / np.sum(np.exp(y))
             p2 = np.argmax(y2)
@@ -65,46 +65,23 @@ def generate_words(seed, input_hidden, hidden_hidden, bh, by, hidden_output, dic
             predicted_ids2.append(predicted_id2)
 
             # If it generates a #END# tag, print the generated sentence
-            if predicted_id == dicts['word_to_id']['#END#']:
-                return dicts['id_to_word'][seed] + ' ' \
-                    + ' '.join([dicts['id_to_word'][x] for x in predicted_ids]) \
-                    + '\n           ' + dicts['id_to_word'][seed] + ' ' \
-                    + ' '.join([dicts['id_to_word'][x] for x in predicted_ids2])
-
+            '''if predicted_id == dicts['word_to_id']['#END#']:
+                if training_type == 'letters':
+                    return dicts['id_to_word'][seed] + ' ' \
+                        + ''.join([dicts['id_to_word'][x] for x in predicted_ids]) \
+                        + '\n           ' + dicts['id_to_word'][seed] + ' ' \
+                        + ''.join([dicts['id_to_word'][x] for x in predicted_ids2])
+                if training_type == 'words':
+                    return dicts['id_to_word'][seed] + ' ' \
+                        + ' '.join([dicts['id_to_word'][x] for x in predicted_ids]) \
+                        + '\n           ' + dicts['id_to_word'][seed] + ' ' \
+                        + ' '.join([dicts['id_to_word'][x] for x in predicted_ids2])'''
         # Returns generated text if max number of samples has been reached.
-        return dicts['id_to_word'][seed] + ' ' + ' '.join([dicts['id_to_word'][x] for x in predicted_ids]) \
-            + '\n            ' + dicts['id_to_word'][seed] + ' ' \
-            + ' '.join([dicts['id_to_word'][x] for x in predicted_ids2[:-1]])
-
-    # Same as words but with letters. Everything is the same unless return statement, I am yet to merge these 2
-    elif training_type == 'letters':
-        if use_word2vec:
-            x = emb[seed]
-        else:
-            x = np.zeros((vocab_size,))
-            x[seed] = 1
-
-        predicted_ids = []
-        h = np.zeros((1, nodes))
-
-        for t in range(samples):
-            h = np.tanh(np.dot(x, input_hidden) + np.dot(hidden_hidden, h[0])+bh)
-            y = np.dot(h, hidden_output) + by
-            p = np.exp(y)/np.sum(np.exp(y))
-            predicted_id = np.random.choice(range(vocab_size), p=p.ravel())
-
-            if use_word2vec:
-                x = emb[predicted_id]
-            else:
-                x = np.zeros((1, vocab_size))
-                x[0][predicted_id] = 1
-
-            predicted_ids.append(predicted_id)
-            if predicted_id == dicts['word_to_id']['#END#']:
-                return dicts['id_to_word'][seed] + ' ' + ''.join([dicts['id_to_word'][x] for x in predicted_ids[:-1]])
-
-        return dicts['id_to_word'][seed] + ''.join([dicts['id_to_word'][x] for x in predicted_ids])
-
-
-    else:
-        print('Incorrect selection of TRAINING_TYPE')
+        if training_type == 'letters':
+            return dicts['id_to_word'][seed] + ' ' + ''.join([dicts['id_to_word'][x] for x in predicted_ids]) \
+                + '\n            ' + dicts['id_to_word'][seed] + ' ' \
+                + ''.join([dicts['id_to_word'][x] for x in predicted_ids2[:-1]])
+        if training_type == 'words':
+            return dicts['id_to_word'][seed] + ' ' + ' '.join([dicts['id_to_word'][x] for x in predicted_ids]) \
+                + '\n            ' + dicts['id_to_word'][seed] + ' ' \
+                + ' '.join([dicts['id_to_word'][x] for x in predicted_ids2[:-1]])
