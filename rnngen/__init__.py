baa15ab92a5145cf2cfe67b941d76b0517afe3b5
Gabriel Petersson <46445785+gabrielpetersson@users.noreply.github.com>
2019-06-25 22:51:07 +0200

    Added default params

--- rnngen/__init__.py
+++ rnngen/__init__.py
@@ -1,6 +1,7 @@
 from rnngen.processing.preprocessing import ProcessData
 from rnngen.recurrentnetwork.trainer import Generator
 from rnngen.word2vec.word2vec import word2vec_trainer
+from rnngen import datasets
 
 
 def pre_process(save_pickle_dir, use_text_dir):
@@ -9,9 +10,9 @@ def pre_process(save_pickle_dir, use_text_dir):
 
 
 def generate(text_dir, use_word2vec=True,
-             emb_dir='embeddings.npy'):
+             embeddings_dir=datasets.SIMPSONS_EMBEDDINGS_300):
     Generator(text_dir, use_word2vec,
-              emb_dir)
+              embeddings_dir)
 
 
 def word2vec(text_dir, save_dir, previously_trained_emb=''):
